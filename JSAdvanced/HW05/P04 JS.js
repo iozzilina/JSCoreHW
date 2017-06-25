ea014fc94ecd9d2fce7739cc5c2b9f614c30b268
iozzilina <iova_dineva@yahoo.com>
2017-06-25 22:58:01 +0300

    HW 05

---
+++ JSAdvanced/HW05/P04 JS.js
@@ -0,0 +1,10 @@
+function attachEvents(){
+
+    $('a.button').on('click', clicked);
+
+    function clicked(){
+        $('.selected').removeClass('selected');    
+        $(this).addClass('selected')    
+    }
+
+}
